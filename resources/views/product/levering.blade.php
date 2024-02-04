2f31aab661dbba8f5b1e37248e9b74b2e0ff3742
KaiOsU8 <336757@student.mboutrecht.nl>
2024-02-04 19:34:23 +0100

    added pagina levering

---
+++ resources/views/product/levering.blade.php
@@ -0,0 +1,32 @@
+<x-app-layout>
+<script src="https://kit.fontawesome.com/344423929d.js" crossorigin="anonymous"></script>
+    <div class=" flex justify-center flex-col items-center">
+        <h1 class="text-center text-4xl m-20">Overzicht Magazijn Jamin</h1>
+            <table class="text-center flex justify-center">
+                <tr class="text-xl">
+                    <th class="border-solid border-2 border-sky-400">Barcode</th>
+                    <th class="border-solid border-2 border-sky-400">Naam</th>
+                    <th class="border-solid border-2 border-sky-400">VerpakkingsEenheid</th>
+                    <th class="border-solid border-2 border-sky-400">AantalAanwezig</th>
+                    <th class="border-solid border-2 border-sky-400">AllergenenInfo</th>
+                    <th class="border-solid border-2 border-sky-400">LeveringsInformatie</th>
+                </tr>
+                @foreach ($products as $product)
+                    @foreach ($product->magazijn as $magazijn)
+                        <tr>
+                            <td class="border-solid border-2 border-sky-400">{{ $product->Barcode }}</td>
+                            <td class="border-solid border-2 border-sky-400">{{ $product->Naam }}</td>
+                            <td class="border-solid border-2 border-sky-400">{{ $magazijn->VerpakkingsEenheid }}</td>
+                            <td class="border-solid border-2 border-sky-400">{{ $magazijn->AantalAanwezig }}</td>
+                            <td class="border-solid border-2 border-sky-400">
+                                <a href="{{ route('product.show', $product) }}"><i class="fa-solid fa-x text-red-600"></i></a>
+                            </td>
+                            <td class="border-solid border-2 border-sky-400">
+                                <a href="{{ route('product.levering', $product) }}"><i class="fa-solid text-blue-500 fa-question"></i></a>
+                            </td>
+                        </tr>
+                    @endforeach
+                @endforeach
+            </table>
+    </div>
+</x-app-layout>
